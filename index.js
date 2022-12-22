0e919e156402975172a93569190431c6b8de3c7a
amirhossainbond <67521933+amirhossainbond@users.noreply.github.com>
2022-12-23 02:17:44 +0600

    api create

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const cors= require('cors')
 const port= process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const product= require('./product.json')
+ require('dotenv').config();
+
+
 //  user:dbuser01 
 //  pass: wwLhTVE3NpkZcLxF
 
@@ -11,22 +14,25 @@ const product= require('./product.json')
 app.use(cors());
 app.use(express.json());
 
+console.log(process.env.DB_USER)
+console.log(process.env.DB_PASS)
 
 
 
-
-const uri = "mongodb+srv://dbuser01:wwLhTVE3NpkZcLxF@cluster0.nxaovwt.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nxaovwt.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run(){
     try{
-          const userCollection =  client.db('nodemongoDB').collection('users');
-          const user = {
-          name: "amirh",
-          email: "amirh@gmail.com"
-          }
-          const result = await userCollection.insertOne(user);
-          console.log(result)
+          const productCollection =  client.db('nodemongoDB').collection('users');
+         app.get('/products', async (req, res) => {
+           const query = {}
+           const cursor = productCollection.find(query);
+           const products= await cursor.toArray()
+           res.send(products)
+
+         })
     }
         
     finally{
